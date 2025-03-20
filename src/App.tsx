69caadc50d5a826615df8cb6baa46ac63974b946
Shakim Harris <amenraprod@gmail.com>
2025-03-20 17:04:18 -0400

    import

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './App.css'
 import Reverse, { reverseStr, reverseString } from './algorithms/reverse';
 import Palindrome from './algorithms/palindrome';
+import maxChar from './algorithms/maxchar';
+import Chunk from './algorithms/arraychunk';
 
 function App() {
  const string = Reverse("Shakim");
@@ -16,5 +18,8 @@ function App() {
 }
 
 Palindrome("kayak");
-console.log(Palindrome("kayak"));
+console.log(Palindrome("codingmoney"));
+console.log(maxChar("aaajjjjjjjjjlllloooop"));
+
+Chunk([1,2,3,4,5,6,7,8,9], 3);
 export default App
