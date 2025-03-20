af91ec0d54f53f38a296e2586c950cff1cf8066d
Shakim Harris <amenraprod@gmail.com>
2025-03-20 17:04:10 -0400

    finished

---
+++ src/algorithms/arraychunk.tsx
@@ -0,0 +1,11 @@
+// Given an array and chunk size, divide the array into subarrays of length size
+
+export default function Chunk (arr: number[], size: number): number[]{
+        const result: any = [];
+        let index = 0;
+        while (index < arr.length) {
+            result.push(arr.slice(index, index + size);
+            index += size;
+        }
+        return result;
+}
