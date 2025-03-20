1f3478ea6f99a05f89d6c1a9b975a0a5b029de83
Shakim Harris <amenraprod@gmail.com>
2025-03-20 17:40:56 -0400

    import

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import Reverse, { reverseStr, reverseString } from './algorithms/reverse';
 import Palindrome from './algorithms/palindrome';
 import maxChar from './algorithms/maxchar';
 import Chunk from './algorithms/arraychunk';
-
+import Capitalize, { Capital } from
+  './algorithms/capitalize';
 function App() {
  const string = Reverse("Shakim");
  console.log(string);
@@ -21,5 +22,7 @@ Palindrome("kayak");
 console.log(Palindrome("codingmoney"));
 console.log(maxChar("aaajjjjjjjjjlllloooop"));
 
-Chunk([1,2,3,4,5,6,7,8,9], 3);
+console.log(Chunk([1,2,3,4,5,6,7,8,9], 3));
+console.log(Capitalize("I'm only here until tonight"));
+console.log(Capital("I'm only here until tonight"));
 export default App
